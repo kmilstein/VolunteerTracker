958eb1a51c113dfb5df3007428d60c65e89974f4
Ksenia Milstein <Ksenia.Milstein@gmail.com>
2019-10-14 00:01:41 -0400

    Added ability to change default image in NewUserView

--- VolunteerTracker/src/views/NewUserViewController.java
+++ VolunteerTracker/src/views/NewUserViewController.java
@@ -11,12 +11,16 @@ import java.util.ResourceBundle;
 import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 import javax.imageio.ImageIO;
 import models.Volunteer;
 
@@ -43,8 +47,42 @@ public class NewUserViewController implements Initializable {
     private File imageFile;
     
     /**
-     * This method 
+     * This method opens a FileChooser object to allow the user to browse for a new
+     * image file. When that is completed, it will update the view to the a new image.
      */
+    public void chooseImageButtonPushed(ActionEvent event) {
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open Item");
+        
+        FileChooser.ExtensionFilter jpgFilter = new FileChooser.ExtensionFilter("Image File (*.jpg)", "*.jpg");
+        FileChooser.ExtensionFilter pngFilter = new FileChooser.ExtensionFilter("Image File (*.png)", "*.png");
+        
+        fileChooser.getExtensionFilters().addAll(jpgFilter, pngFilter);
+        
+        String userDirectoryString = System.getProperty("user.home")+"\\Pictures";
+        File userDirectory = new File(userDirectoryString);
+        
+        if (!userDirectory.canRead()) {
+            userDirectory = new File(System.getProperty("user.home"));
+        }
+        fileChooser.setInitialDirectory(userDirectory);
+        
+        imageFile = fileChooser.showOpenDialog(stage);
+        
+        if (imageFile.isFile()) {
+            try {
+                BufferedImage bufferedImage = ImageIO.read(imageFile);
+                Image img = SwingFXUtils.toFXImage(bufferedImage, null);
+                imageView.setImage(img);
+            }
+            catch (IOException e) {
+                System.err.println(e.getMessage());
+            }
+        }
+        
+    }
     
     /**
      * This method will read from the scene and try to create a new instance of a Volunteer
