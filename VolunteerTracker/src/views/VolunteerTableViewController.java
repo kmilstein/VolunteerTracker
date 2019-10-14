33b933d391afbecb5b19cd3f0943f190f75070e8
Ksenia Milstein <Ksenia.Milstein@gmail.com>
2019-10-14 02:42:59 -0400

    Loaded in volunteer data

--- VolunteerTracker/src/views/VolunteerTableViewController.java
+++ VolunteerTracker/src/views/VolunteerTableViewController.java
@@ -1,10 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package views;
 
+import java.io.File;
 import java.net.URL;
 import java.time.LocalDate;
 import java.util.ResourceBundle;
@@ -12,7 +8,11 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import models.Volunteer;
+import java.sql.*;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
 /**
  * FXML Controller class
@@ -26,7 +26,7 @@ public class VolunteerTableViewController implements Initializable {
     @FXML
     private TableColumn<Volunteer, Integer> volunteerIDColumn;
     @FXML
-    private TableColumn<Volunteer, String> FirstNameColumn;
+    private TableColumn<Volunteer, String> firstNameColumn;
     @FXML
     private TableColumn<Volunteer, String> lastNameColumn;
     @FXML
@@ -35,10 +35,63 @@ public class VolunteerTableViewController implements Initializable {
     private TableColumn<Volunteer, LocalDate> birthdayColumn;
     /**
      * Initializes the controller class.
+     * @param url
+     * @param rb
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+        volunteerIDColumn.setCellValueFactory(new PropertyValueFactory<>("volunteerID"));
+        firstNameColumn.setCellValueFactory(new PropertyValueFactory<>("firstName"));
+        lastNameColumn.setCellValueFactory(new PropertyValueFactory<>("lastName"));
+        phoneNumberColumn.setCellValueFactory(new PropertyValueFactory<>("phoneNumber"));
+        birthdayColumn.setCellValueFactory(new PropertyValueFactory<>("birthday"));
     
+        try {
+            loadVolunteers();
+        }
+        catch(SQLException e) {
+            System.err.println(e.getMessage());
+        }
+    }       
+
+    /**
+     * This method will load the volunteers from the database and load them into
+     * the TableView object
+     * @throws java.sql.SQLException
+     */
+     public void loadVolunteers() throws SQLException {
+         ObservableList<Volunteer> volunteers = FXCollections.observableArrayList();
+         
+         Connection conn = null;
+         Statement statement = null;
+         ResultSet resultSet = null;
+         try {
+             conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/volunteer?autoReconnect=true&useSSL=false", "root", "MySQLPassword1");
+             statement = conn.createStatement();
+             
+             resultSet = statement.executeQuery("SELECT * FROM volunteers");
+             
+             while (resultSet.next()) {
+                 Volunteer newVolunteer = new Volunteer(resultSet.getString("firstName"),
+                                                         resultSet.getString("lastName"),
+                                                         resultSet.getString("phoneNumber"),
+                                                         resultSet.getDate("birthday").toLocalDate());
+                 newVolunteer.setVolunteerID(resultSet.getInt("volunteerID"));
+                 newVolunteer.setImageFile(new File(resultSet.getString("imageFile")));
+                 volunteers.add(newVolunteer);
+             }
+             volunteerTable.getItems().addAll(volunteers);
+         }
+         catch (Exception e) {
+             System.err.println(e.getMessage());
+         }
+         finally {
+            if (conn != null)
+                conn.close();
+            if (statement != null)
+                statement.close();
+            if (resultSet != null)
+                resultSet.close();
+         }
+    }
 }
